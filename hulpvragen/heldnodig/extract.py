31affd6975a21f92c795d0162e449d96000b95bb
milovanderlinden <milo@dogodigi.net>
2020-03-23 13:23:49 +0100

    Prettier

--- hulpvragen/heldnodig/extract.py
+++ hulpvragen/heldnodig/extract.py
@@ -12,12 +12,17 @@ results = soup.find(class_='container')
 questions = results.find_all(class_='card')
 hulpvragen = []
 for card in questions:
-    title = card.find('h5').text
-    subtitle = card.find('h6').text
-    description = card.find('p', class_='card-text').text
-    hulpvragen.append({'title': title, 'location': subtitle, 'description': description})
+    title = card.find('h5').text.strip(' \t\n\r')
+    rawlocation = card.find('h6').text.strip(' \t\n\r')
+    #remove (maps) from rawlocation, split on first space
+    rawlocation = rawlocation.strip('(Maps)')
+    locationobject = rawlocation.split(' ', 1)
+    location = { 'postalcode': locationobject[0].strip(), 'city': locationobject[1].strip(), 'title': rawlocation.strip()}
+
+    description = card.find('p', class_='card-text').text.strip(' \t\n\r')
+    hulpvragen.append({'category': title, 'location': location, 'description': description})
 with open('../heldnodig.json', 'w') as f:
-    json.dump(hulpvragen, f)
+    json.dump(hulpvragen, f, skipkeys=True, ensure_ascii=True, indent=2, separators=None, default=None, sort_keys=True)
 
 
 
